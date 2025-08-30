93fdd7aa0ba9c8ecb02b1cdb3c57efa5f5cd9aae
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-30 11:16:10 -0600

    Add User and Note models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+from .extensions import db, bcrypt
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, raw_password):
+        self.password_hash = bcrypt.generate_password_hash(raw_password).decode()
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password_hash, raw_password)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text)
