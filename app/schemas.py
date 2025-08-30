959183f5322e86c7e01e8ed528c797dd940e0bfd
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-30 11:16:24 -0600

    Add Marshmallow schemas for User and Note

---
+++ app/schemas.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    email = fields.Email(required=True)
+
+class NoteSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    body = fields.Str()
+
+user_schema = UserSchema()
+note_schema = NoteSchema()
+notes_schema = NoteSchema(many=True)
